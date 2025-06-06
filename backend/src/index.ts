52035e9b578b60f89c5c2c765ea83bc63cc49309
Felix Bandl <inf24227@lehre.dhbw-stuttgart.de>
2025-06-06 14:24:12 +0200

    add Login mit Token erstellung

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,21 @@
-import cors from "cors";             
-import authRouter from "./routes/authRouter";
-import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import authRouter from "./routes/authRouter";
+import protectedRouter from "./routes/protectedRouter";
+
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-// CORS Middleware konfigurieren
-app.use(cors({
-  origin: "http://localhost:3000",  
-  credentials: true,                 
-}));
-
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api", protectedRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
