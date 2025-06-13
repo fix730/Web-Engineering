3c50b0b51d8ce1e1da60aa70cce3d5b01bbf4dc4
Leo <jeffBezos@amazon.com>
2025-06-13 22:29:46 +0200

    Intergreierung von Posts gescheit in homepage, posts können jetzt angeklickt werden

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,9 @@ import { getUser, logout } from "../slices/authSlice";
 import axiosInstance from "../api/axiosInstance";
 import Header from "./components/Header/Header";
 import { useState } from "react";
+import Post from "./components/Post/Post";
+
+
 function Home() {
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
@@ -14,8 +17,8 @@ function Home() {
     const dummyPosts = [
         {
             id: 1,
-            title: "Sonnenuntergang am Sewee",
-            description: "Wunderschöner Sonnenuntergang am nächsten Sewee",
+            title: "Sonnenuntergang am Seweewew",
+            description: "Wunderschöner Sonnenuntergang am nächsten Seweewe",
             location: "München",
             imageUrl: "https://ih1.redbubble.net/image.4665578724.8271/raf,360x360,075,t,fafafa:ca443f4786.jpg",
         },
@@ -72,27 +75,11 @@ function Home() {
     <Header />
 
     {dummyPosts.map((post) => (
-              <div
-            key={post.id}
-            className="max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden mb-6 cursor-pointer flex flex-col md:flex-row border border-gray-200"
-            onClick={() => setCurrentPost(post)}
-          >
-            {/* Image on the left */}
-            <div className="md:w-1/3 w-full">
-              <img
-                src={post.imageUrl}
-                alt={post.title}
-                className="w-full h-full object-cover"
-              />
-            </div>
-
-            {/* Text content on the right */}
-            <div className="md:w-2/3 w-full p-6 border-t md:border-t-0 md:border-l border-gray-200 flex flex-col justify-center">
-              <h2 className="text-2xl font-bold text-gray-900 mb-2">{post.title}</h2>
-              <p className="text-gray-700 mb-2">{post.description}</p>
-              <p className="text-gray-500">Location: {post.location}</p>
-            </div>
-          </div>
+            <Post
+              key={post.id}
+              post={post}
+              onClick={() => setCurrentPost(post)} // Klick auf Post öffnet Modal
+            />
     ))}
     {/* Für das Anschauen von den Posts / draufclicken */}
     {currentPost && (
