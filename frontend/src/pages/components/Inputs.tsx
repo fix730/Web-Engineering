9741d279d3c1578c766c77eebbb256e47e9c918a
Felix Bandl <inf24227@lehre.dhbw-stuttgart.de>
2025-06-17 09:29:23 +0200

    Input geändert

--- frontend/src/pages/components/Inputs.tsx
+++ frontend/src/pages/components/Inputs.tsx
@@ -15,7 +15,7 @@ type DateProps = {
     date: string;
     handleChnceDate: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
-const classNameStandart = "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6";
+const classNameStandart = "block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 border border-gray-300 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-indigo-600 focus:ring-1 sm:text-sm/6";
 
 export function Paasswort({ password, handleChncePassword, autoComplete }: PassordProps) {
     return (
